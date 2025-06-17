355f966b6006d670c2dce6d020d05a9a16d8faa0
ostsam <ost.sam@gmail.com>
2025-06-16 22:46:43 -0400

    interface tinkering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { useChat } from "@ai-sdk/react";
+import { CircleLoader } from "react-spinners";
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { messages, input, handleInputChange, handleSubmit, status, stop } =
+    useChat();
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages.map((message) => (
@@ -17,15 +19,28 @@ export default function Chat() {
           })}
         </div>
       ))}
-
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
+      <div className="fixed bottom-18">
+        {(status === "submitted" || status === "streaming") && (
+          <div>
+            {status === "submitted" && (
+              <CircleLoader color="#1E88E5" size={25} />
+            )}
+            <button type="button" onClick={() => stop()}>
+              Stop
+            </button>
+          </div>
+        )}
+        <form onSubmit={handleSubmit}>
+          <input
+            className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
+            value={input}
+            placeholder="Say something..."
+            onChange={handleInputChange}
+            disabled={status !== "ready"}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
