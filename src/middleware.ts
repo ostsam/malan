ad79a5d81f7ef881689007507781a1c51ecc0073
ostsam <ost.sam@gmail.com>
2025-06-19 15:08:56 -0400

    automatically go to dashboard if logged in, login page if not

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
- 
+
 export async function middleware(request: NextRequest) {
-	const sessionCookie = getSessionCookie(request);
- 
-	if (!sessionCookie) {
-		return NextResponse.redirect(new URL("/login", request.url));
-	}
- 
-	return NextResponse.next();
+  // If the request is for the root path, redirect to the dashboard.
+  if (request.nextUrl.pathname === "/") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  const sessionCookie = getSessionCookie(request);
+
+  if (!sessionCookie) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
 }
- 
+
 export const config = {
-	matcher: ["/dashboard"], // Specify the routes the middleware applies to
+  matcher: ["/", "/dashboard"], // Specify the routes the middleware applies to
 };
