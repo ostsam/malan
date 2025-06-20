52c0d47b07c0181c4a6f698f4cbc40d3cea8e4d8
ostsam <ost.sam@gmail.com>
2025-06-20 12:12:42 -0400

    pls work

---
+++ src/app/api/chat/chat/[id]/loading.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import ChatPageSkeleton from "../../../../ui/ChatPageSkeleton";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return <ChatPageSkeleton />;
+}
