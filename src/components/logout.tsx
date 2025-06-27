9217b84679715684777345b0e734ab2533973cd0
ostsam <ost.sam@gmail.com>
2025-06-27 14:44:03 -0400

    attempt

--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -2,15 +2,13 @@
 import { authClient } from "@/lib/auth-client"; 
 import { LogOutIcon } from "lucide-react";
 
-export function LogoutButton() {
-    const handleLogout = async () => {
-        await authClient.signOut();
-        window.location.href = '/'
-    }
+export default async function LogoutButton() {
+    await authClient.signOut();
+    window.location.href = '/'
 
     return (
         <button 
-        className="flex items-center justify-center py-3 gap-2 rounded-lg text-md font-medium text-gray-900 hover:bg-gray-100"            onClick={handleLogout}
+        className="flex items-center justify-center py-3 gap-2 rounded-lg text-md font-medium text-gray-900 hover:bg-gray-100"            onClick={LogoutButton}
         >
             Sign out
             <LogOutIcon className="h-4 w-4"/>
