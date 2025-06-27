e189243fd475a159b7d0a6f7a0d5dbf0d3e9b000
ostsam <ost.sam@gmail.com>
2025-06-27 14:50:40 -0400

    baseurl attempt

--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -2,13 +2,15 @@
 import { authClient } from "@/lib/auth-client"; 
 import { LogOutIcon } from "lucide-react";
 
-export default async function LogoutButton() {
-    await authClient.signOut();
-    window.location.href = '/'
+export function LogoutButton() {
+    const handleLogout = async () => {
+        await authClient.signOut();
+        window.location.href = '/'
+    }
 
     return (
         <button 
-        className="flex items-center justify-center py-3 gap-2 rounded-lg text-md font-medium text-gray-900 hover:bg-gray-100"            onClick={LogoutButton}
+        className="flex items-center justify-center py-3 gap-2 rounded-lg text-md font-medium text-gray-900 hover:bg-gray-100"            onClick={handleLogout}
         >
             Sign out
             <LogOutIcon className="h-4 w-4"/>
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,10 +1,19 @@
 import { createAuthClient } from "better-auth/react";
 
+const getBaseURL = () => {
+    if (typeof window !== 'undefined') {
+      return window.location.origin;
+    }
+    if (process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL) {
+        return `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`;
+    }
+    if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+      return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    }
+    return 'http://localhost:3000';
+  };
+  
 export const authClient = createAuthClient({
-    baseURL: typeof window !== "undefined"
-    ? window.location.origin
-    : process.env.NEXT_PUBLIC_VERCEL_URL
-        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-        : "http://localhost:3000"
-})
+    baseURL: getBaseURL(),
+});
 export const { signIn, signUp, useSession, signOut } = authClient;
