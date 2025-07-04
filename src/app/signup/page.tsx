f31d9b31af1a8b3e95cbbdd49fe5e7235a83d9eb
ostsam <ost.sam@gmail.com>
2025-07-03 22:33:16 -0400

    mild tinkering

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,7 +4,10 @@ export default function SignUpPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-2">
-        <a href="/" className="flex items-center self-center font-medium">
+        <a
+          href="/"
+          className="flex items-center self-center font-medium mb-2.5"
+        >
           <img src="/malan-caps.svg" alt="Malan Logo" className="h-12 w-auto" />
         </a>
         <SignUpForm />
