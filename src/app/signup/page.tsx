2a63000c333036086ae10395b2b46bf24874c068
ostsam <ost.sam@gmail.com>
2025-07-05 09:30:14 -0400

    ui tweaks

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,7 +2,7 @@ import { SignUpForm } from "@/components/signup-form";
 
 export default function SignUpPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-2">
         <a href="/" className="flex items-center self-center font-medium">
           <img src="/malan-caps.svg" alt="Malan" className="h-12 w-auto" />
